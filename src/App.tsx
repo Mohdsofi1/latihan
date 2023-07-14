34d65fd9250ba3e80810851faa6c1320a508ea20
Mohdsofi1 <mestilahhh@gmail.com>
2023-07-14 15:08:41 +0800

    first

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // update h1 with increment of 1
+  const [number, setNumber] = useState(0);
+  //press button to console log error
+  function handleButtonClick(delta: number) {
+    setNumber(number + delta);
+  }
+
+  return (
+    <div className="background">
+      <div>
+        <h1>{number}</h1>
+      </div>
+      <button onClick={() => handleButtonClick(+10)}>+10</button>
+      <button onClick={() => handleButtonClick(-1)}>-1</button>
+      <button onClick={() => handleButtonClick(+1)}>+1</button>
+      <button onClick={() => handleButtonClick(-10)}>-10</button>
+    </div>
+  );
+}
+
+export default App;
